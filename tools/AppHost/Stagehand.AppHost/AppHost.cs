605db442051f0f3d382bca1b96aaefcc12ba624b
Nitish Bhat <nitishbhatwork@gmail.com>
2026-06-13 14:57:59 +0100

    dev: add keycloak and auth support for k8 + aspire

--- tools/AppHost/Stagehand.AppHost/AppHost.cs
+++ tools/AppHost/Stagehand.AppHost/AppHost.cs
@@ -4,10 +4,18 @@ var postgres = builder.AddPostgres("postgres")
     .WithDataVolume("stagehand-postgres-data")
     .WithLifetime(ContainerLifetime.Persistent);
 
+var keycloak = builder.AddContainer("keycloak", "quay.io/keycloak/keycloak", "26.1")
+.WithHttpEndpoint(port: 8081, targetPort: 8080, name: "http")
+.WithArgs("start-dev", "--import-realm")
+.WithEnvironment("KC_BOOTSTRAP_ADMIN_USERNAME", "admin")
+.WithEnvironment("KC_BOOTSTRAP_ADMIN_PASSWORD", "admin")
+.WithBindMount("../../../deploy/platform/keycloak/base", "/opt/keycloak/data/import");
+
 var catalogDb = postgres.AddDatabase("catalog");
 
 builder.AddProject<Projects.Stagehand_Catalog_Api>("catalog-api")
     .WithReference(catalogDb)
-    .WaitFor(catalogDb);
+    .WaitFor(catalogDb)
+    .WaitFor(keycloak);
 
 builder.Build().Run();
